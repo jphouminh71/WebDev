d8b6b5be6e08ac4ec87cab1d1f5520cfaf914bfa
JonathanPhouminh <jonathanphouminh@Jonathans-MacBook-Pro.local>
2021-02-27 20:52:16 -0700

    removed comments

--- Authentication/app.js
+++ Authentication/app.js
@@ -55,72 +55,44 @@ userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model('User', userSchema);
 
 
-
-
-/* Setup Routes */
-app.get('/', (req, res) => {
-    res.render('home');
-});
-
-
-app.get('/login', (req, res) => {
-    res.render('login');
-});
-
-app.get('/register', (req, res) => {
-    res.render('register')
-});
-
-
 app.post('/register', (req, res) => {
-    
+    /* Grab the pass/email they passed in form  */
+    const newUser = User({
+        email: req.body.username,
+        password: req.body.password
+    });
+
+    newUser.save(function(err){
+        if (err){
+            console.log(err);
+        }
+        else {
+            res.render('secrets')
+        }
+    })
 });
 
 
 app.post('/login', (req, res) => {
-    
+    /* We need to check if this user belongs in our db */
+    const userName = req.body.username; 
+    const password = req.body.password; 
+
+    User.findOne({email:userName},function(err, foundUser){
+        if (err) {
+            console.log(err)
+        } else {
+            if (foundUser) {
+                if (foundUser.password == password) {
+                    res.render('secrets');
+                }
+                else {
+                    console.log("BAD INPUT");
+                }
+            }
+        }
+    }); 
 });
-
-
-/* COMMENETING OUT TO REWRITE FOR COOKIES AND SESSIONS APPROACH */
-// app.post('/register', (req, res) => {
-//     /* Grab the pass/email they passed in form  */
-//     const newUser = User({
-//         email: req.body.username,
-//         password: req.body.password
-//     });
-
-//     newUser.save(function(err){
-//         if (err){
-//             console.log(err);
-//         }
-//         else {
-//             res.render('secrets')
-//         }
-//     })
-// });
-
-
-// app.post('/login', (req, res) => {
-//     /* We need to check if this user belongs in our db */
-//     const userName = req.body.username; 
-//     const password = req.body.password; 
-
-//     User.findOne({email:userName},function(err, foundUser){
-//         if (err) {
-//             console.log(err)
-//         } else {
-//             if (foundUser) {
-//                 if (foundUser.password == password) {
-//                     res.render('secrets');
-//                 }
-//                 else {
-//                     console.log("BAD INPUT");
-//                 }
-//             }
-//         }
-//     }); 
-// });
 
 app.listen(3000, () => {
     console.log(`Server started on port: 3000`);
